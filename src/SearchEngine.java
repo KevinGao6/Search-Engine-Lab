408f93ef02e26cb08bece2f9b913d139c0663072
eomuratoglu <33694486+eomuratoglu@users.noreply.github.com>
2018-04-21 10:57:12 -0400

    more stuff

--- src/SearchEngine.java
+++ src/SearchEngine.java
@@ -1,4 +1,12 @@
+import java.util.List;
+import java.util.Map;
 
-public class SearchEngine {
-
+public class SearchEngine 
+{
+	//holds the name for the url
+	private String myURL;
+	//holds the word index
+	private Map<String, List<String>> myIndex;
+	
+	
 }
