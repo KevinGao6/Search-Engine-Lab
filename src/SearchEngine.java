11245b784fa1d008bbe67e616792e5485d3a2cdf
KevinGao6 <32113541+KevinGao6@users.noreply.github.com>
2018-04-21 11:45:54 -0400

    Final Version

--- src/SearchEngine.java
+++ src/SearchEngine.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -9,19 +8,22 @@ import java.util.TreeSet;
 /**
  * 
  * @author Kevin, Ezra
- *
+ * Date: 4/21/2018
+ * 
+ * This class models a Search Engine for text files - 'Giggle'. 
  */
 public class SearchEngine 
 {
 	//The name for the url (file name)
 	private String myURL;
+	
 	//The word index
 	private Map<String, List<String>> myIndex;
 	
 	/**
 	 * Constructor for SearchEngine
-	 * Saves url in myUrl; initializes myIndex to an empty HashMap with an initial capacity of 20,000
-	 * @param url
+	 * saves url in myUrl
+	 * initializes myIndex to an empty HashMap with an initial capacity of 20,000
 	 */
 	public SearchEngine(String url)
 	{
@@ -30,6 +32,7 @@ public class SearchEngine
 	}
 	
 	/**
+	 * Used in the displaying of the name of the file in which hits were found
 	 * @return myUrl
 	 */
 	public String getURL()
@@ -43,19 +46,26 @@ public class SearchEngine
 	 */
 	public void add(String line)
 	{
+		//Obtain TreeSet<String> of each word in line
 		TreeSet<String> words = (TreeSet<String>) parseWords(line);
 		
+		//For each word
 		for (String word: words)
 		{
+			//Attempt to retrieve the list associated with the key 'word' in myIndex
 			List<String> list = myIndex.get(word);
 			
+			//Successfully found object associated with key
 			if(list != null)
 			{
+				//Add the line
 				list.add(line);
 			}
 			
+			//myIndex does not have the key 'word'
 			else
 			{
+				//Create and add new LinkedList<String> to put into the Hashmap
 				List<String> newList = new LinkedList<String>();
 				newList.add(line);
 				myIndex.put(word, newList);
@@ -76,9 +86,13 @@ public class SearchEngine
 	 */
 	private Set<String> parseWords(String line)
 	{
+		//Result to return
 		TreeSet<String> result = new TreeSet<String>();
 		
+		//Obtain array of all individual words in line
 		String[] words = line.split("\\W+");
+		
+		//Add each word to the resulting array, in lowercase
 		for (String word : words)
 			result.add(word.toLowerCase());
 		
