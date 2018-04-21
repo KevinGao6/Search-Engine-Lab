4ceb084d6b808e6f20653b5bc6e239462147e50a
KevinGao6 <32113541+KevinGao6@users.noreply.github.com>
2018-04-21 11:04:31 -0400

    Method Headers

--- src/SearchEngine.java
+++ src/SearchEngine.java
@@ -1,12 +1,27 @@
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * 
+ * @author Kevin, Ezra
+ *
+ */
 public class SearchEngine 
 {
-	//holds the name for the url
+	//The name for the url
 	private String myURL;
-	//holds the word index
+	//The word index
 	private Map<String, List<String>> myIndex;
 	
+	/**
+	 * Saves url in myUrl; initializes myIndex to an empty HashMap with an initial capacity of 20,000
+	 * @param url
+	 */
+	public SearchEngine(String url)
+	{
+		this.myURL = url;
+		myIndex = new HashMap<String, List<String>>(20000);
+	}
 	
 }
